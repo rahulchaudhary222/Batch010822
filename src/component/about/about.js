bb2b54838e3571ac117c29c1e41b92d797dbe3a3
chaudharysunny163 <chaudharysunny163@gmail.com>
2022-09-25 19:07:56 +0530

    sdfs

--- src/component/about/about.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div>THIS IS ABOUT</div>;
-};
-
-export default About;
--- src/component/contacts/contact.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-  return <div>THIS IS Contact</div>;
-};
-export default Contact;
