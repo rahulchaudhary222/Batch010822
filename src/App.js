5fb721d8599428b9c808b6941230834b8a68b9fa
chaudharysunny163 <chaudharysunny163@gmail.com>
2022-09-25 19:07:00 +0530

    sunny

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import Home from "./component/home/home";
-import Contact from "./component/contacts/contact";
-import About from "./component/about/about";
+
 function App() {
   return (
     <div className="App">
