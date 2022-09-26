68c2a86409e113a5b0adc83a3ebdd91d2a4b843e
chaudharysunny163 <chaudharysunny163@gmail.com>
2022-09-26 11:52:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/home/home";
-import About from "./components/about/about";
-import Contact from "./components/contact/conatct";
-import { useNavigate } from "react-router-dom";
+
 function App() {
-  const navigate = useNavigate();
   return (
     <div className="App">
-      <div className="navBar">
-        <span
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          home
-        </span>
-        <span
-          onClick={() => {
-            navigate("/contact");
-          }}
-        >
-          contact
-        </span>
-        <span
-          onClick={() => {
-            navigate("/about");
-          }}
-        >
-          about
-        </span>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+      <h1>title added by rahul</h1>
+      <p>new paragraph</p>
     </div>
   );
 }
