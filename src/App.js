64ad3ae979eb6b5c6737bf341459b5504c88892f
chaudharysunny163 <chaudharysunny163@gmail.com>
2022-09-29 18:18:08 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/home";
+import About from "./components/about/about";
+import Contact from "./components/contact/conatct";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <h1>heading from sunny</h1>
-      <h1>heading from sunny</h1>
+      <div className="navBar">
+        <span
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          home
+        </span>
+        <span
+          onClick={() => {
+            navigate("/contact");
+          }}
+        >
+          contact
+        </span>
+        <span
+          onClick={() => {
+            navigate("/about");
+          }}
+        >
+          about
+        </span>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
