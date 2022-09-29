a806457c7a9a4d944bf6d1ecf0b30a149706a8c8
chaudharysunny163 <chaudharysunny163@gmail.com>
2022-09-29 18:20:03 +0530

    about

---
+++ src/components/about/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>this is About page</h3>
+      <div>Adding About page content for dummy pull request.</div>
+    </div>
+  );
+};
+export default About;
